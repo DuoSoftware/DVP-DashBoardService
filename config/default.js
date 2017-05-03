8598d8a8f6c526035494576ca60ed66524fe1bf7
heshanindika <k.heshan.indika@gmail.com>
2017-05-03 17:25:31 +0530

    update default.json

--- config/default.js
+++ config/default.js
@@ -65,7 +65,7 @@ module.exports = {
     },
     "Services" : {
         "accessToken":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdWtpdGhhIiwianRpIjoiYWEzOGRmZWYtNDFhOC00MWUyLTgwMzktOTJjZTY0YjM4ZDFmIiwic3ViIjoiNTZhOWU3NTlmYjA3MTkwN2EwMDAwMDAxMjVkOWU4MGI1YzdjNGY5ODQ2NmY5MjExNzk2ZWJmNDMiLCJleHAiOjE5MDIzODExMTgsInRlbmFudCI6LTEsImNvbXBhbnkiOi0xLCJzY29wZSI6W3sicmVzb3VyY2UiOiJhbGwiLCJhY3Rpb25zIjoiYWxsIn1dLCJpYXQiOjE0NzAzODExMTh9.Gmlu00Uj66Fzts-w6qEwNUz46XYGzE8wHUhAJOFtiRo",
-        "notificationServiceHost": "192.168.5.186",
+        "notificationServiceHost": "notificationservice.app.veery.cloud",
         "notificationServicePort": "8089",
         "notificationServiceVersion": "1.0.0.0"
     }
